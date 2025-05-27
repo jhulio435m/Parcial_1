dae3fd96e3650cd5bbd9058bd60f5489c3c73a73
jhulio435m <76138183+jhulio435m@users.noreply.github.com>
2025-05-27 17:53:08 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Parcial_1/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
